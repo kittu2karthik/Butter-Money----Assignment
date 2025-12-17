0bc9538b7f851f2164cef06452ff2faeb23e6da5
kittu2karthik <kittu2karthik@gmail.com>
2025-12-18 02:22:00 +0530

    Added Basic Global Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import GlobalStyle from './styles/GlobalStyles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UsersPage from './pages/UsersPage.jsx';
+import UserPage from './pages/UserPage.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
-import React from 'react'
 
 function App() {
   console.log(import.meta.env.VITE_API_BASE_URL);
   return (
-    <div>App</div>
+    <>
+    <GlobalStyle />
+
+     <BrowserRouter>
+     <Routes>
+      <Route index element={<UsersPage/>} />
+      <Route path="/user/:id" element={<UserPage/>} />
+
+       <Route path="*" element={<PageNotFound />} />
+     </Routes>
+     </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+
+  transition: color 0.3s, background-color 0.3s;
+  min-height: 100vh;
+  line-height: 1.5;
+  font-size: 1.6rem;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+  color: inherit;
+}
+
+button {
+  cursor: pointer;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+  hyphens: auto;
+}
+
+img {
+  max-width: 100%;
+}
+
+`;
+
+export default GlobalStyles;
